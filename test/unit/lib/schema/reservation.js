c9732ba776727448e6477900d72c2d2a38fde463
srikanth <srikanthsree50@gmail.com>
2018-07-30 13:50:55 +0530

    last commit

--- test/unit/lib/schema/reservation.js
+++ test/unit/lib/schema/reservation.js
@@ -1,8 +1,21 @@
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
 const chai = require('chai');
 const should = chai.should();
 const Reservation = require('../../../../lib/schema/reservation');
 
 describe('Reservation Schema', function() {
+  const debugStub = function() {
+    return sinon.stub();
+  }
+let reservations;
+
+before(function() {
+  reservations = proxyquire('../../../../lib/reservations', {
+    debug: debugStub
+  });
+});
+
   context('Date and Time Combination', function() {
     it('should return a ISO 8601 date and time with valid input ', function() {
       const date = '2017/06/10';
@@ -19,6 +32,40 @@ describe('Reservation Schema', function() {
       // should.not.exist(Reservation.combineDateTime(date,time));
       Reservation.combineDateTime(date,time)
       .should.not.equal('2017-06-10T06:02:00.000Z');
+    });
+  });
+
+  context('Validator', function() {
+    it('should pass a valid reservation with no optional fields', function(done) {
+      const reservation = new Reservation({
+        date:'2017/06/10',
+        time:'o6:02 AM',
+        party:4,
+        name:'Family',
+        email:'user@user.com'
+      });
+
+      reservation.validator(function(error,value) {
+        value.should.deep.equal(reservation);
+        done(error);
+      });
+    });
+
+    it('should fail a invalid reservation with bad email', function(done) {
+      const reservation = new Reservation({
+        date:'2017/06/10',
+        time:'o6:02 AM',
+        party:4,
+        name:'Family',
+        email:'user'
+      });
+
+      reservation.validator(function(error) {
+        error.should
+        .be.an('error')
+        .and.not.be.null;
+        done();
+      });
     });
   });
 });
